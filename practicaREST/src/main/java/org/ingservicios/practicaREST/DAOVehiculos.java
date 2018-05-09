7b0303c03d102be1fa98c836e9c08321e8383af9
antonio <antonio@10.82.198.227>
2018-05-09 10:16:16 +0200

    Añadidas dependencias en el pom.xml. -->Conector MySQL. -->RowMapper.

--- practicaREST/src/main/java/org/ingservicios/practicaREST/DAOVehiculos.java
+++ practicaREST/src/main/java/org/ingservicios/practicaREST/DAOVehiculos.java
@@ -2,8 +2,16 @@ package org.ingservicios.practicaREST;
 
 import javax.sql.DataSource;
 
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 public class DAOVehiculos {
-//hola
+	//Añadir libreria spring-jdbc y dependencia junto a la versión
+	public JdbcTemplate jdbcTemplate;
+	//private DataSource dataSource;
+	@Autowired
+	public void setDataSource(DataSource dataSource) {
+	this.jdbcTemplate = new JdbcTemplate(dataSource);
+	}
+	
 }
