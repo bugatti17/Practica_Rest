b9ad5d87f3be0093278f1a8814d5e4e52faf3b1e
antonio <antonio@10.82.198.227>
2018-05-09 10:09:39 +0200

    Commit inicial. Creación de proyecto. Añadido DTO y DAO

--- practicaREST/src/main/java/org/ingservicios/practicaREST/DAOVehiculos.java
+++ practicaREST/src/main/java/org/ingservicios/practicaREST/DAOVehiculos.java
@@ -2,9 +2,8 @@ package org.ingservicios.practicaREST;
 
 import javax.sql.DataSource;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
+
 
 public class DAOVehiculos {
-
+//hola
 }
